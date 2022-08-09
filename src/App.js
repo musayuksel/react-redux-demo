f8ea26a5eb88532225fb96208fd986b4c736f29b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-08-09 12:54:46 +0100

    Implement Router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import './App.css';
-import { Box, Typography } from '@mui/material';
-import { useSelector, useDispatch } from 'react-redux';
-import { AuthenticatedUser, UnAuthenticatedUser } from './components';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { AuthenticatedUser, UnAuthenticatedUser } from "./components";
+import NavBar from "./components/AuthenticatedUser/NavBar";
+import Cart from "./components/AuthenticatedUser/Cart";
 
 const App = () => {
-
-  // [REDUX HOOK]
-  // useSelector is a hook that allows us to access the state of the redux store
-  // you can use it more than once in a component
-  const user = useSelector(state => state.user);
-
   return (
-    <Box
-      p={10}
-    > 
-      <Typography variant='h3'
-        sx={{
-          fontWeight: 'bold',
-        }}
-        mb={2}
-      >
-        Redux Demo 
-      </Typography>
-
-      { user.isAuthenticated &&
-        <AuthenticatedUser />
-      }
-
-      { !user.isAuthenticated &&
-        <UnAuthenticatedUser />
-      }
-    </Box>
-  )
-}
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<UnAuthenticatedUser />} />
+        <Route path="/:userName" element={<AuthenticatedUser />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider as ReduxProvider } from 'react-redux';
-import store from './redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <BrowserRouter>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
